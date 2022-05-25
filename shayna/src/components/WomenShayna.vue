1614bb14543894b2a8e063afbf52ff917b09c151
Muhammad Aswan <muhaswan190897@gmail.com>
2022-05-25 18:53:28 +0800

    update shayna

---
+++ shayna/src/components/WomenShayna.vue
@@ -0,0 +1,103 @@
+<template>
+        <!-- Women Banner Section Begin -->
+    <section class="women-banner spad">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12 mt-5" v-if="products.length > 0">
+                    <carousel class="product-slider" :items="3" :autoplay="true" :nav="false" :dots="false" :loop="true">
+                        <div class="product-item" v-for="itemProduct in products" v-bind:key="itemProduct.id">
+                            <div class="pi-pic">
+                                <img v-bind:src="itemProduct.galleries[0].photo" alt="" height="450px" />
+                                <ul>
+                                    <li @click="saveKeranjang(itemProduct.id, itemProduct.name, itemProduct.price, itemProduct.galleries[0].photo)" class="w-icon active">
+                                        <a href="#"><i class="icon_bag_alt"></i></a>
+                                    </li>
+                                    <li class="quick-view">
+                                        <router-link v-bind:to="'/product/'+itemProduct.id">+ Quick View</router-link>                                                
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="pi-text">
+                                <div class="catagory-name">{{ itemProduct.type }}</div>
+                                <router-link to="/product">
+                                    <a href="#">
+                                        <h5>{{ itemProduct.name }}</h5>
+                                    </a>
+                                </router-link>
+                                <div class="product-price">
+                                    ${{ itemProduct.price }}
+                                    <span>$35.00</span>
+                                </div>
+                            </div>
+                        </div>
+                    </carousel>
+                </div>
+
+                <div class="col-lg-12" v-else>
+                    <p>
+                        Produk terbaru belum tersedia untuk saat ini.
+                    </p>
+                </div>
+
+            </div>
+        </div>
+    </section>
+    <!-- Women Banner Section End -->
+</template>
+
+<script>
+import carousel from 'vue-owl-carousel';
+import axios from 'axios';
+
+export default {
+    name: 'WomenShayna',
+    components: {
+        carousel,
+        },
+        data() {
+            return {
+                products: [],
+                keranjangUser: []
+            };
+        },
+        mounted() {
+            
+            axios
+            .get("http://127.0.0.1:8000/api/products")
+            .then(res => (this.products = res.data.data.data))
+            .catch(err => console.log(err))
+        
+            if(localStorage.getItem('keranjangUser')){
+                try{
+                    this.keranjangUser = JSON.parse(localStorage.getItem('keranjangUser'));
+                } catch(e){
+                    localStorage.removeItem('keranjangUser');
+                }
+            }
+        },
+        methods:{
+            saveKeranjang(idProduct, nameProduct, priceProduct, photoProduct){
+
+            var productStored = {
+                "id": idProduct,
+                "name": nameProduct,
+                "price": priceProduct,
+                "photo": photoProduct,
+                }
+
+            this.keranjangUser.push(productStored);
+            const parse = JSON.stringify(this.keranjangUser);
+            localStorage.setItem("keranjangUser", parse);
+
+            window.location.reload();
+            },
+        },
+};
+</script>
+
+<style scoped>
+.product-item{
+    margin-right: 12px;
+    margin-left: 12px;
+}
+</style>
---
+++ shayna/src/views/Product.vue
@@ -0,0 +1,136 @@
+<template>
+  <div class="product">
+    <HeaderShayna/>
+     <!-- Breadcrumb Section Begin -->
+    <div class="breacrumb-section text-left">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="breadcrumb-text product-more">
+                        <router-link to="/"><i class="fa fa-home"></i> Home</router-link>
+                        <span>Detail</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Breadcrumb Section Begin -->
+
+      <!-- Product Shop Section Begin -->
+    <section class="product-shop spad page-details">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="product-pic-zoom">
+                                <img class="product-big-img" :src="gambar_default" alt="" />
+                            </div>
+                            <div class="product-thumbs" v-if="productDetails.galleries.length > 0">
+                                <carousel class="product-thumbs-track ps-slider" :nav="false" :dots="false">
+                                    <div v-for="ss in productDetails.galleries" :key="ss.id" class="pt" @click="changeImage(ss.photo)" :class="ss.photo == gambar_default ? 'active' : '' ">
+                                        <img :src="ss.photo" alt="" />
+                                    </div>
+                                </carousel>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="product-details text-left">
+                                <div class="pd-title">
+                                    <span>{{ productDetails.type }}</span>
+                                    <h3>{{ productDetails.name }}</h3>
+                                </div>
+                                <div class="pd-desc">
+                                    <p v-html="productDetails.description"></p>                                   
+                                    <h4>${{ productDetails.price }}</h4>
+                                </div>
+                                <div class="quantity">
+                                    <router-link to="/cart">
+                                    <a @click="saveKeranjang(productDetails.id, productDetails.name, productDetails.price, productDetails.galleries[0].photo)" href="#" class="primary-btn pd-cart">Add To Cart</a>
+                                    </router-link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Product Shop Section End -->
+    <RelatedShayna/>
+    <FooterShayna/>
+  </div>
+</template>
+
+<script>
+import HeaderShayna from "@/components/HeaderShayna.vue";
+import RelatedShayna from "@/components/RelatedShayna.vue";
+import FooterShayna from "@/components/FooterShayna.vue";
+import carousel from 'vue-owl-carousel';
+
+import axios from "axios";
+
+export default {
+  name: "Product",
+  components: {
+    // HelloWorld,
+    HeaderShayna,
+    RelatedShayna,
+    FooterShayna,
+    carousel,
+  },
+  data(){
+      return{
+        gambar_default:"",
+        productDetails:[],
+        keranjangUser:[]
+      }
+  },
+  methods:{
+      changeImage(urlImage){
+          this.gambar_default = urlImage;        
+      },
+      setDataPicture(data){
+          this.productDetails = data;
+          this.gambar_default = data.galleries[0].photo;
+      },
+      saveKeranjang(idProduct, nameProduct, priceProduct, photoProduct){
+
+          var productStored = {
+              "id": idProduct,
+              "name": nameProduct,
+              "price": priceProduct,
+              "photo": photoProduct,
+          }
+
+          this.keranjangUser.push(productStored);
+          const parse = JSON.stringify(this.keranjangUser);
+          localStorage.setItem('keranjangUser', parse);
+      },
+  },
+  mounted() {
+      if(localStorage.getItem('keranjangUser')){
+          try{
+              this.keranjangUser = JSON.parse(localStorage.getItem('keranjangUser'));
+          } catch(e){
+              localStorage.removeItem('keranjangUser');
+          }
+      }
+        axios
+        .get("http://127.0.0.1:8000/api/products",{
+            params:{
+                id: this.$route.params.id
+            }
+        })
+        .then(res => (this.setDataPicture(res.data.data)))
+        .catch(err => console.log(err))
+        }
+};
+</script>
+
+<style scoped>
+.product-thumbs .pt{
+    margin-right: 7px;
+    margin-left: 7px;
+}
+</style>
