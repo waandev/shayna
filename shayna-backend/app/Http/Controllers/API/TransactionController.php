a22619a128129b62bc9e964fb362b65f3a24a50a
Muhammad Aswan <muhaswan190897@gmail.com>
2022-05-25 18:48:49 +0800

    upload shayna

---
+++ shayna-backend/app/Http/Controllers/API/TransactionController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Transaction;
+
+class TransactionController extends Controller
+{
+    public function get(Request $request, $id)
+    {
+        $product = Transaction::with(['details.product'])->find($id);
+
+        if ($product)
+            return ResponseFormatter::success($product, 'Data transaksi berhasil diambil');
+        else
+            return ResponseFormatter::error(null, 'Data transaksi tidak ada', 404);
+    }
+}
---
+++ shayna-backend/app/Http/Controllers/ProductController.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\HTTP\Requests\ProductRequest;
+
+use Illuminate\Support\Str;
+use App\Models\Product;
+use App\Models\ProductGallery;
+
+class ProductController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $items = Product::all();
+
+        return view('pages.products.index')->with([
+            'items' => $items
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('pages.products.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        $data['slug'] = Str::slug($request->name);
+
+        Product::create($data);
+        return redirect()->route('products.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $item = Product::findOrFail($id);
+
+        return view('pages.products.edit')->with([
+            'item' => $item
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(ProductRequest $request, $id)
+    {
+        $data = $request->all();
+        $data['slug'] = Str::slug($request->name);
+
+        $item = Product::findOrFail($id);
+        $item->update($data);
+
+        return redirect()->route('products.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $item = Product::findOrFail($id);
+        $item->delete();
+
+        ProductGallery::where('products_id', $id)->delete();
+
+        return redirect()->route('products.index');
+    }
+
+    public function gallery(Request $request, $id)
+    {
+        $products = Product::findOrFail($id);
+        $items = ProductGallery::with('product')->where('products_id', $id)->get();
+
+        return view('pages.products.gallery')->with([
+            'product' => $products,
+            'items' => $items
+        ]);
+    }
+}
